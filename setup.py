da2ec6946fbe6cbe08246e6ca2af1374740ff152
Robert Gibboni <galileo@gmail.com>
2018-07-10 22:50:56 -0700

    added setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='openoasis',
+    version='0.1',
+    description='OpenOakland openoasis',
+    url='https://github.com/pssachdeva/openoasis',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']))
